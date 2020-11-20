b2a9d3bbdb7734cf4d16f1ca601fbb097aea492b
Vimbi <i.am.vimbi@gmail.com>
2020-11-20 18:08:16 +0300

    wrong tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,12 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (Array.isArray(arr)) {
-    if(arr.indexOf('--discard-next') !== -1) {
-      return arr.map()
+  
+  if (arr.some(el => el === 'DEF')) {
+    console.log(arr);
+  }
 
-    }
-  } else {
-    return false;
+  if (Array.isArray(arr)) {
+
+    let newArr = arr.slice();
+
+    if(newArr.every(elem => elem !== '--discard-next' && elem !== '--discard-prev' && elem !== '--double-next' && elem !== '--double-prev')) {
+      return newArr;
+    } else {
+      newArr.forEach((value, index) => {
+        if(value === '--discard-next') {
+          newArr[index + 1] !== undefined ? newArr.splice(index, 2) : newArr.splice(index, 1);
+        } else if (value === '--discard-prev') {
+          index - 1 >= 0 ? newArr.splice(index - 1, 2) : newArr.splice(index, 1);
+        } else if (value === '--double-next') {
+          newArr[index + 1] !== undefined ? newArr[index] = newArr[index + 1] : newArr.splice(index, 1);
+        } else if (value === '--double-prev') {
+          index - 1 >=0 ? newArr[index] = newArr[index - 1] : newArr.splice(index, 1);
+        }
+      });
+  
+      return transform(newArr);
+    } 
+    
+  } else {    
+    throw Error;
   }
 };
