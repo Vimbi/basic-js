3680646cb17665852c571ce1593e4d0e46793ddd
Vimbi <i.am.vimbi@gmail.com>
2020-12-14 21:03:06 +0300

    carbon dating done, start what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(Object.prototype.toString.call(date) === '[object Date]') {
+    let x = date.getMonth;
+  } else throw Error;
+
+  switch (x) {
+    case 0:
+    case 1:
+    case 12:
+      return 'winter';
+      break;
+
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+      break;
+    
+    case 8:
+    case 9:
+    case 10:
+      return 'fall';
+      break;
+  } 
 };
