b416d2c3f2582fbe6f0aed9d3b32f3bca002db86
Vimbi <i.am.vimbi@gmail.com>
2020-12-15 20:38:19 +0300

    hanoi tower and what-season done

--- src/what-season.js
+++ src/what-season.js
@@ -1,33 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if(Object.prototype.toString.call(date) === '[object Date]') {
-    let x = date.getMonth;
-  } else throw Error;
-
-  switch (x) {
-    case 0:
-    case 1:
-    case 12:
-      return 'winter';
-      break;
-
-    case 2:
-    case 3:
-    case 4:
-      return 'spring';
-      break;
-
-    case 5:
-    case 6:
-    case 7:
-      return 'summer';
-      break;
+  if(date === undefined) {
+    return 'Unable to determine the time of year!'
+  } else if(Object.prototype.toString.call(date) === '[object Date]') {
     
-    case 8:
-    case 9:
-    case 10:
-      return 'fall';
-      break;
-  } 
+    switch (date.getMonth()) {
+      case 0:
+      case 1:
+      case 11:
+        return 'winter';
+        break;
+  
+      case 2:
+      case 3:
+      case 4:
+        return 'spring';
+        break;
+  
+      case 5:
+      case 6:
+      case 7:
+        return 'summer';
+        break;
+      
+      case 8:
+      case 9:
+      case 10:
+        return 'fall';
+        break;
+    } 
+  } else throw Error; 
 };
